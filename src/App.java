b7cfa8206fce5df630aefaed27d019b9313e5b4a
AFC <albertflori2007@gmail.com>
2026-09-22 20:22:35 +0200

    mapa e inici

--- src/App.java
+++ src/App.java
@@ -2,12 +2,38 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
 
         App p = new App();
         p.principal();
 
+    }
+
+    public void inicialitzar() {
+        
+        // objectes de cada un dels ítems que s'utilitzen al joc
+        Objectes llanterna = new Objectes("Llanterna", "x",
+                "Petita llanterna de color negre amb un botó per poder encendre la llum.", true);
+        Objectes targetaPersonal = new Objectes("Targeta personal", "Oficina principal",
+                "Petita targeta identificadora que porta la foto del capità.", true);
+        Objectes targetaCompany = new Objectes("Targeta del company", "Dormitoris",
+                "Una targeta d'accés que pertany a un altre tripulant de la nau.", true);
+        Objectes vestitAstronauta = new Objectes("Vestit d'astronauta", "Vestuaris",
+                "És un vestit blanc amb un casc gran i rodo.", true);
+        Objectes donuts = new Objectes("Dónuts", "Cuina",
+                "Una caixa de dònuts acabats de fer i amb una bona capa de sucre. Fan una olor deliciosa que no només temptaria qualsevol membre de la tripulació afamat.",
+                true);
+        Objectes einaDelTaller = new Objectes("Eina del taller", "Taller",
+                "Petita caixa d’eines antiga. Es pot utilitzar per reparar la nau!", true);
+    }
+
+    public void menuP() {
+        System.out.println("========== MENÚ INICIAL ==========");
+        System.out.println("1. Iniciar una partida nova");
+        System.out.println("2. Sortir del joc ");
+        System.out.println("==================================");
+    
     }
 
     public void principal(){
@@ -18,43 +44,40 @@ public class App {
         // variable que permeti finalitzar el joc (sortir del bucle del joc)
         boolean jugant = true;
 
-        while (!jugant) {
+        while (jugant) {
 
-            // objectes de cada un dels ítems que s'utilitzen al joc
-            Objectes llanterna = new Objectes("Llanterna", "x",
-                    "Petita llanterna de color negre amb un botó per poder encendre la llum.", true);
-            Objectes targetaPersonal = new Objectes("Targeta personal", "Oficina principal",
-                    "Petita targeta identificadora que porta la foto del capità.", true);
-            Objectes targetaCompany = new Objectes("Targeta del company", "Dormitoris",
-                    "Una targeta d'accés que pertany a un altre tripulant de la nau.", true);
-            Objectes vestitAstronauta = new Objectes("Vestit d'astronauta", "Vestuaris",
-                    "És un vestit blanc amb un casc gran i rodo.", true);
-            Objectes donuts = new Objectes("Dónuts", "Cuina",
-                    "Una caixa de dònuts acabats de fer i amb una bona capa de sucre. Fan una olor deliciosa que no només temptaria qualsevol membre de la tripulació afamat.",
-                    true);
-            Objectes einaDelTaller = new Objectes("Eina del taller", "Taller",
-                    "Petita caixa d’eines antiga. Es pot utilitzar per reparar la nau!", true);
-            
-            System.out.println("========== MENÚ INICIAL ==========");
-            System.out.println("1. Iniciar una partida nova");
-            System.out.println("2. Sortir del joc ");
-            System.out.println("==================================");
+            inicialitzar();
+
+            menuP();
             int opcioMenu = llegirEnters();
             switch (opcioMenu) {
                 case 1:
-                    System.out.println("OPCIO11");
+                    //INICI DEL JOC
+                    System.out.println("La nau PIAXII explora l'espai inhòspit en direcció al planeta SUMMEM on es creu que hi poden haver les condicions idònies per arrelar una nova vida.");
+                    System.out.println("Desrpés d'un llarg període d'hivernació provocada pel llarg viatge, el capità Bond es desperta del son induït...");
+                    System.out.println("iHall: Que tal ha dormit capità Bond? Em sap greu destorbar-lo però he detectat una anomalia a la nau. S'ha de reparar immediatament");
+                    System.out.println("Bond: Coi de ferralla intel·ligent!");
+                    System.out.println("En Bond està altament irritat ja que el somni no podia ser d'allò més excitant i realista...");
+                    System.out.println("Bond: Espero que hagi estat indispensable que haver-me destorbat del son! Sinó prepara't per que et programi unes quantes sessions de Treball cooperatiu amb un pedagog inspirat...");
+                    System.out.println("iHall: Li garanteixo capità que no és una falsa alarma, no m'arriscaria a patir un càstig similar...");
+                    System.out.println("Bond: Està bé iHall, dona'm l'informe de la situació i anem per feina!");
+                    System.out.println("iHall: Aqui va l'informe... Resulta que hem xocat contre un petit aeròlit que ha provocat petits danys al sistema de propulsió de la nau els quals necessiten intervenció humana.");
+                    System.out.println("iHall: El teu objectiu és fer-te amb l'eina de reparació que està al taller de la nau i reparar els danys dels propulsors de la nau.");
+                    System.out.println("Aquest és el mapa de la nau: ");
+                    mostrarMapa();
                     break;
                 case 2: 
-                    System.out.println("OPCIO2");
+                    jugant = false;
                     break;
                 default:
                     System.out.println("Torna-ho a intentar!");
                     break;
             }
+        }
     }
-}
-//funció per llegir nombres enters
-    public int llegirEnters(){
+
+    // funció per llegir nombres enters
+    public int llegirEnters() {
         int nombre = 0;
         try {
             nombre = sc.nextInt();
@@ -65,4 +88,31 @@ public class App {
         }
         return nombre;
     }
+    public void mostrarMapa() {
+    System.out.println(
+        "                 ┌───────────────┐\n" +
+        "                 │    TALLERS    │\n" +
+        "                 └───────┬───────┘\n" +
+        "┌────────────────────────┼────────────────────────┐\n" +
+        "│ VESTUARI               │ OFICINES               │ BANYS\n" +
+        "│                        │                        │\n" +
+        "├───────────────┐    ┌───┴───┐    ┌───────────────┤\n" +
+        "│ CUINA         │    │COMAN- │    │ DORMITORI     │\n" +
+        "│               │    │DAMENT │    │               │\n" +
+        "└───────────────┘    └───┬───┘    └───────────────┘\n" +
+        "                         │\n" +
+        "                 ┌───────┴───────┐\n" +
+        "                 │    MENJADOR   │\n" +
+        "                 └───────┬───────┘\n" +
+        "                 ┌───────┴───────┐\n" +
+        "                 │ SALA SORTIDA  │\n" +
+        "                 │    EXTERIOR   │\n" +
+        "                 └───────┬───────┘\n" +
+        "                     ┌───┴───┐\n" +
+        "                     │PROPULS│\n" +
+        "                     │  ORS  │\n" +
+        "                     └───────┘"
+    );
 }
+}
+
